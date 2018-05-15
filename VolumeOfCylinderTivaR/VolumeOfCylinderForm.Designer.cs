06a55d162ed747cc8aafc23f83ccfaf8271e4f09
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-15 12:37:46 -0400

    Initial commit

---
+++ VolumeOfCylinderTivaR/VolumeOfCylinderForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace VolumeOfCylinderTivaR
+{
+    partial class frmVolumeOfCylinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(43, 48);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(46, 13);
+            this.lblRadius.TabIndex = 0;
+            this.lblRadius.Text = "Radius: ";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(46, 114);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(44, 13);
+            this.lblHeight.TabIndex = 1;
+            this.lblHeight.Text = "Height: ";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(134, 48);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 2;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(134, 114);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(144, 164);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(90, 73);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Volume";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmVolumeOfCylinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 264);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblRadius);
+            this.Name = "frmVolumeOfCylinder";
+            this.Text = "Volume Of a Cylinder By Tiva Rait";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ VolumeOfCylinderTivaR/VolumeOfCylinderForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 14-05-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #31 - Volume of a Cylinder
+ * This program 
+*/
+
+namespace VolumeOfCylinderTivaR
+{
+    public partial class frmVolumeOfCylinder : Form
+    {
+        public frmVolumeOfCylinder()
+        {
+            InitializeComponent();
+
+        }
+
+        // Function: CalculateVolume
+        // Input: double radius, double height
+        // Output: double volume
+        // Description:
+        private double CalculateVolume(double radius, double height)
+        {
+            // Define const
+            const double PI = 3.14;
+
+            double volume;
+
+            volume = (PI * Math.Pow(radius, 2) * height);
+
+            return volume;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Declare local variables
+            double radius, height;
+
+            // Get the values from text boxes
+            radius = Convert.ToDouble(txtRadius.Text);
+            height = Convert.ToDouble(txtHeight.Text);
+
+            // Call function
+
+        }
+    }
+}
